b02042cce7ee72a6d74d5b9fc5b606b3c50c8481
jonathan <jtmacoco@mail.csuchico.edu>
2023-10-27 21:30:49 -0700

    added home page animation(fadeIn)

--- personal_websit_v2/app/page.tsx
+++ personal_websit_v2/app/page.tsx
@@ -1,17 +1,20 @@
+"use client"
 import Home from './sections/Home';
 import Skills from './sections/Skills';
 import Navbar from './components/navbar';
+import { motion } from "framer-motion"
 import './globals.css'
 export default function Page() {
   return (
     <>
       <div className='relative'>
         <Navbar />
-        <div id="home">
+        <div
+          id="home">
           <Home />
         </div>
         <div id="skills">
-          <Skills/>
+          <Skills />
         </div>
       </div>
     </>
--- personal_websit_v2/app/sections/Home.tsx
+++ personal_websit_v2/app/sections/Home.tsx
@@ -1,10 +1,22 @@
 "use client"
 import { useEffect, useState } from 'react';
 import { about } from '../constants/info';
+import { motion, useAnimation } from "framer-motion"
+import { useInView } from "react-intersection-observer";
 import Image from "next/image";
 import personalWeb from "../images/personalWeb.png"
 import '../globals.css'
 export default function Home() {
+  const controls = useAnimation();
+  const [ref, inView] = useInView()
+  useEffect(() => {
+    if (inView) {
+      controls.start({ opacity: 1 })
+    }
+    else {
+      controls.start({ opacity: 0 })
+    }
+  },[controls,inView])
   const [text, setText] = useState('');
   const delta = 100
   const hello = "\"Hello, I'm Jonathan Macoco a software developer.\"";
@@ -19,7 +31,12 @@ export default function Home() {
   })
   return (
     <>
-      <main className="home w-full h-full">
+      <motion.main
+        ref={ref}
+        initial={{opacity: 0}}
+        animate={controls}
+        transition={{ delay: .5 }}
+        className="home w-full h-full">
         <div className='absolute top-40'>
           {/*add personal photo here potentialy*/}
         </div>
@@ -27,7 +44,7 @@ export default function Home() {
           <p className="text-white text-lg sm:text-2xl text-center  pb-20 ">{text}</p>
           <p className='text-white text-md text-center sm:text-xl pb-20'>{about}</p>
         </div>
-      </main>
+      </motion.main>
     </>
   );
 }
