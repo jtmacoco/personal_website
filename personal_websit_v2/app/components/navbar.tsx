6e66823ab2e678da19068e9c775ea2002f2ad14c
jonathan <jtmacoco@mail.csuchico.edu>
2023-10-29 10:34:06 -0700

    change mobile navbar animation

--- personal_websit_v2/app/components/navbar.tsx
+++ personal_websit_v2/app/components/navbar.tsx
@@ -4,12 +4,13 @@ import { navLinks } from "../constants/info";
 import Image from "next/image";
 import github from "../images/github_logo_white.png"
 import linkedin from "../images/linkedin_logo.png"
-import email from "../images/email_logo.png"
+import { motion } from "framer-motion"
 import Link from 'next/link'
 import { FiMenu } from "react-icons/fi";
 import { VscChromeClose } from "react-icons/vsc";
 export default function Navbar() {
     const [close, setClose] = useState(false);
+    const [reload,setReload] = useState(true);
     const scrollToSection = (id: string): void => {
         const section = document.getElementById(id) as HTMLElement | null;
         if (section) {
@@ -21,6 +22,12 @@ export default function Navbar() {
         const updateClose = !close;
         setClose(updateClose);
         localStorage.setItem("close", JSON.stringify(updateClose))
+    }
+    const menuVariants = {
+        hidden: { x: 0 },
+        visible: { x: "-100%", 
+        transition: { delay: .5 },
+     }
     }
     return (
         <nav className="bg-black sticky top-0 z-50 h-14 flex items-center justify-center">
@@ -36,26 +43,6 @@ export default function Navbar() {
                     ))}
                 </ul>
             </div>
-
-            <div className="absolute left-4 sm:hidden">
-                <button onClick={toggleMenu}>
-                    <FiMenu size={30} color="white" className={`${close ? "hidden" : "block"}`} />
-                    <VscChromeClose size={30} color="white" className={`${!close ? "hidden" : "block"}`} />
-                </button>
-                <div
-                    className={`${!close ? "hidden" : "block"}`}>
-                    <ul className="justify-center items-center h-10 px-2 bg-black flex flex-row gap-4 rounded-md absolute top-12 ">
-                        {navLinks.map((link) => (
-                            <li key={link.id}
-                                className="nav-line">
-                                <button onClick={(e) => { e.preventDefault(); scrollToSection(link.id) }}>
-                                    {link.title}
-                                </button>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </div >
             <ul className="absolute right-5 flex flex-row gap-4">
                 <li>
                     <Link href="https://github.com/jtmacoco">
@@ -68,6 +55,31 @@ export default function Navbar() {
                     </Link>
                 </li>
             </ul>
+            <div className="absolute left-4 sm:hidden">
+                <button onClick={()=>{toggleMenu();setReload(false);}}>
+                    <FiMenu size={30} color="white" className={`${close ? "hidden" : "block"}`} />
+                </button>
+                <motion.div
+                    initial={"hidden"}
+                    animate={close ? "hidden" : "visible"}
+                    variants={menuVariants}
+                    className={`${reload ? "hidden":"block"} fixed top-0 left-0 bg-black bg-cover h-screen w-screen `}>
+                    <button onClick={toggleMenu} className="relative top-2 left-2">
+                        <VscChromeClose size={30} color="white" className={`${!close ? "hidden" : "block"}`} />
+                    </button>
+                    <ul className="pb-16 justify-center items-center flex flex-col gap-16 rounded-md my-auto h-screen">
+                        {navLinks.map((link) => (
+                            <li key={link.id}
+                                className="nav-line">
+                                <button onClick={(e) => { e.preventDefault(); scrollToSection(link.id); toggleMenu() }}>
+                                    {link.title}
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </motion.div>
+            </div >
+
         </nav >
     );
 }
--- personal_websit_v2/app/sections/Skills.tsx
+++ personal_websit_v2/app/sections/Skills.tsx
@@ -17,7 +17,7 @@ export default function skills() {
                 <div className="bg-white h-[4px] w-full"></div>
             </h1>
             <div className="container flex items-center justify-center mx-auto h-screen">
-                <ul className="grid grid-cols-3 grid-rows-2 sm:grid-cols-4 pt-8 sm:pt-0 sm:grid-rows-4 sm:gap-28 ">
+                <ul className="grid grid-cols-3 grid-rows-2 sm:grid-cols-4 pt-24 sm:pt-0 sm:grid-rows-4 gap:2 sm:gap-28 ">
                     {skill.map((s, index) => (
                         <motion.li
                             variants={fadInVariants}
