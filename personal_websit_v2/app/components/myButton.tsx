0f3a02b67dfc27fd3cb304630c82ee33fbd8f568
jonathan <jtmacoco@mail.csuchico.edu>
2023-10-28 19:58:52 -0700

    added resume and contact button components to home page

---
+++ personal_websit_v2/app/components/myButton.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+interface MyButtonProps {
+    title: string;
+}
+const MyButton: React.FC<MyButtonProps> = ({title}) => {
+    return (
+        <a href="#_" className="relative px-5 py-3 overflow-hidden font-medium text-black bg-white border border-gray-600 rounded-lg shadow-inner group">
+            <span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-gray-600 group-hover:w-full ease"></span>
+            <span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-gray-600 group-hover:w-full ease"></span>
+            <span className="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-black group-hover:h-full ease"></span>
+            <span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-black group-hover:h-full ease"></span>
+            <span className="absolute inset-0 w-full h-full duration-300 delay-300 bg-black opacity-0 group-hover:opacity-100"></span>
+            <span className="relative transition-colors duration-300 delay-200 group-hover:text-white ease">{title}</span>
+        </a>
+    )
+}
+export default MyButton;
--- personal_websit_v2/app/sections/Home.tsx
+++ personal_websit_v2/app/sections/Home.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { about } from '../constants/info';
 import { motion, useAnimation } from "framer-motion"
 import { useInView } from "react-intersection-observer";
+import MyButton from '../components/myButton';
 import Image from "next/image";
 import personalWeb from "../images/personalWeb.png"
 import '../globals.css'
@@ -16,7 +17,7 @@ export default function Home() {
     else {
       controls.start({ opacity: 0 })
     }
-  },[controls,inView])
+  }, [controls, inView])
   const [text, setText] = useState('');
   const delta = 100
   const hello = "\"Hello, I'm Jonathan Macoco a software developer.\"";
@@ -33,7 +34,7 @@ export default function Home() {
     <>
       <motion.main
         ref={ref}
-        initial={{opacity: 0}}
+        initial={{ opacity: 0 }}
         animate={controls}
         transition={{ delay: .5 }}
         className="home w-full h-full">
@@ -41,6 +42,10 @@ export default function Home() {
           {/*add personal photo here potentialy*/}
         </div>
         <div className="relative container">
+          <div className="flex items-center justify-center mx-auto gap-4 pb-10">
+            <MyButton title="Resume"/>
+            <MyButton title="Contact"/>
+          </div>
           <p className="text-white text-lg sm:text-2xl text-center  pb-20 ">{text}</p>
           <p className='text-white text-md text-center sm:text-xl pb-20'>{about}</p>
         </div>
