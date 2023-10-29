01dbe4bd8971540a02de49ac213920eae003fb46
jonathan <jtmacoco@mail.csuchico.edu>
2023-10-28 20:23:35 -0700

    skills page mobile responsive

--- personal_websit_v2/app/sections/Skills.tsx
+++ personal_websit_v2/app/sections/Skills.tsx
@@ -12,12 +12,12 @@ export default function skills() {
     }
     return (
         <main className="skills">
-            <h1 className="relative pl-10 top-24 text-4xl">
+            <h1 className="relative sm:pl-10 top-16 sm:top-24 sm:text-4xl">
                 Skills
                 <div className="bg-white h-[4px] w-screen"></div>
             </h1>
             <div className="container flex items-center justify-center mx-auto h-screen">
-                <ul className="grid grid-cols-4 grid-rows-4 gap-28">
+                <ul className="grid grid-cols-3 grid-rows-2 sm:grid-cols-4 pt-8 sm:pt-0 sm:grid-rows-4 sm:gap-28 ">
                     {skill.map((s, index) => (
                         <motion.li
                             variants={fadInVariants}
@@ -25,9 +25,9 @@ export default function skills() {
                             whileInView="animate"
                             custom={index}
                             key={s.title}>
-                            <div className="skills-image mx-auto pt-4">
-                                <Image src={s.src} alt={s.title} height={50} width={50}
-                                    className="mx-auto" />
+                            <div className="skills-image mx-auto pt-3 sm:pt-4">
+                                <Image src={s.src} alt={s.title} 
+                                    className="mx-auto sm:h-14 sm:w-14 h-8 w-8"  />
                             </div>
                             <p className="text-center">{s.title}</p>
                         </motion.li>
