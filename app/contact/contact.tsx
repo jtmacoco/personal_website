cdc70c88857bd2d553c17c7cda68ad3d064ada50
jtmacoco <jtmacoco@gmail.com>
2025-10-06 05:00:09 -0700

    update: updated emailjs

--- app/contact/contact.tsx
+++ app/contact/contact.tsx
@@ -26,16 +26,14 @@ export default function Contact() {
 
         if (name && email && messages) {
             emailjs.send(
-                "service_p8nq2il",
-                "template_4klxjmh",
+                process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
+                process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
                 {
                     from_name: name,
-                    to_name: 'Jonathan',
                     form_email: email,
-                    to_email: 'jtmacoco.jobs@gmail.com',
                     message: messages
                 },
-                "qjMyh4PRr4cIefjVq"
+                process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!
             ).then(() => {
                 setMissingName(false)
                 setMissingNameText(false)
